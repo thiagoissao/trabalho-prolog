719b59408703751cae96f4d26ab1b494fa9347f8
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2021-02-28 12:41:30 -0300

    Diagnóstico de pacientes

--- code.pl
+++ code.pl
@@ -1,6 +1,8 @@
 :- use_module(library(plunit)).
 :- use_module(library(lists)).
 :- consult(probabilidade).
+:- consult(file_manager).
+
 
 sintoma('agressão').
 sintoma('adormecimento no rosto').
@@ -39,31 +41,62 @@ sintoma('tosse seca').
 sintoma('tosse com catarro').
 sintoma('vômito').
 
+
 doenca('ansiedade', ['fadiga', 'suor excessivo', 'náusea', 'coração acelerado']).
-
 doenca('asma', ['ansiedade', 'tosse seca', 'dor no peito']).
-
 doenca('autismo', ['agressão', 'grito', 'apatia']).
-
 doenca('avc', ['dor de cabeça', 'adormecimento no rosto', 'tontura', 'perda de visão']).
-
 doenca('depressão', ['ansiedade', 'apatia', 'insônia', 'fadiga', 'medo', 'insegurança']).
-
 doenca('diabetes', ['fome constante', 'formigamento', 'perda de peso', 'sede']).
-
 doenca('hipertensão arterial', ['dor de cabeça', 'dor no peito', 'dor no braço', 'tontura']).
-
 doenca('meningite', ['dor de cabeça', 'cansaço','febre alta', 'náusea']).
-
 doenca('obesidade', ['artrite', 'dor muscular', 'pressão alta', 'falta de ar']).
+doenca('pneumonia', ['tosse seca', 'tosse com catarro', 'falta de ar', 'febre alta', 'dor no peito', 'dor de cabeça']).
 
-doenca('pneumonia', ['tosse seca', 'tosse com catarro', 'falta de ar', 'febre', 'dor no peito', 'dor de cabeça']).
+
+show_doencas_e_probabilidades(Sintomas) :-
+    write(Sintomas),
+    doenca('ansiedade', Ansiedade),
+    doenca('asma', Asma),
+    doenca('autismo', Autismo),
+    doenca('avc', Avc),
+    doenca('depressão', Depressao),
+    doenca('diabetes', Diabetes),
+    doenca('hipertensão arterial', HipertensaoArterial),
+    doenca('meningite', Meningite),
+    doenca('obesidade', Obesidade),
+    doenca('pneumonia', Pneumonia),
+    get_probabilidade_by_sintomas(Sintomas, Ansiedade, AnsiedadeProb),
+    get_probabilidade_by_sintomas(Sintomas, Asma, AsmaProb),
+    get_probabilidade_by_sintomas(Sintomas, Autismo, AutismoProb),
+    get_probabilidade_by_sintomas(Sintomas, Avc, AvcProb),
+    get_probabilidade_by_sintomas(Sintomas, Depressao, DepressaoProb),
+    get_probabilidade_by_sintomas(Sintomas, Diabetes, DiabetesProb),
+    get_probabilidade_by_sintomas(Sintomas, HipertensaoArterial, HipertensaoArterialProb),
+    get_probabilidade_by_sintomas(Sintomas, Meningite, MeningiteProb),
+    get_probabilidade_by_sintomas(Sintomas, Obesidade, ObesidadeProb),
+    get_probabilidade_by_sintomas(Sintomas, Pneumonia, PneumoniaProb),
+    nl, write('O resultado do protótipo é apenas informativo e que o paciente deve consultar um médico para obter um diagnóstico correto e preciso'), nl,
+    nl, nl,
+    write('Ansiedade: '), write(AnsiedadeProb), nl,
+    write('Asma: '), write(AsmaProb), nl,
+    write('Autismo: '), write(AutismoProb), nl,
+    write('Avc: '), write(AvcProb), nl,
+    write('Depressão: '), write(DepressaoProb), nl,
+    write('Diabetes: '), write(DiabetesProb), nl,
+    write('Hipertensão Arterial: '), write(HipertensaoArterialProb), nl,
+    write('Meningite: '), write(MeningiteProb), nl,
+    write('Obesidade: '), write(ObesidadeProb), nl,
+    write('Pneumonia: '), write(PneumoniaProb),
+    nl, nl.
 
 home :- write('\n ----Escolha alguma opção---- \n'),
         write('0. Consultar paciente\n'),
         write('1. Inserir Paciente \n'),
         write('2. Alterar Paciente \n'),
         write('3. Deletar Paciente \n'),
+        write('4. Diagnosticar Paciente \n'),
+        write('5. Listar todos os Pacientes \n'),
         write('100. Sair\n'),
         read(Op),
         execute(Op).
@@ -72,31 +105,47 @@ execute(Op) :- Op == 0, write('Consultando...\n'), consultar, home;
                Op == 1, write('Inserindo...\n'), inserir,  home;
                Op == 2, write('Alterando...\n'), alterar, home;
                Op == 3, write('Deletando...\n'), deletar, home;
+               Op == 4, write('Diagnosticando...\n'), diagnosticar, home;
+               Op == 5, write('Listando todos os pacientes...\n'), listar_todos, home;
                Op == 100, true.
 
 get_nome(Nome) :- write('Digite o nome do paciente'), read(Nome).
 
-ler_arquivo :-
-    open('pacientes.txt', read, F),
-    ler_pacientes(F, Pacientes),
-    close(F),
-    write(Pacientes), nl.
+ler_sintomas(Sintomas, Entrada) :-
+    write('Sintoma (Digite end. para parar) '),
+    read(Sintoma),
+    (
+        Sintoma = end -> reverse(Sintomas, Entrada);
+        ler_sintomas(Sintomas, [Sintoma | Entrada])
+    ).
 
-ler_pacientes(F, []) :- at_end_of_stream(F).
-ler_pacientes(F, [X | L]) :-
-              \+ at_end_of_stream(F),
-              read(F, X),
-              ler_pacientes(F, L).
 
 consultar :-
     get_nome(Nome),
-    write(Nome).
+    find_paciente(Nome), write('Paciente encontrado!'), nl;
+    write('Não encontramos este paciente'), nl.
 
-inserir :- get_nome(Nome), write(Nome).
+inserir :-
+    get_nome(Nome),
+    find_paciente(Nome).
 
 alterar :- get_nome(Nome), write(Nome).
 
 deletar :- get_nome(Nome), write(Nome).
+
+diagnosticar :-
+    get_nome(Nome),
+    (   find_paciente(Nome) ->
+        ler_sintomas(Sintomas, []),
+        show_doencas_e_probabilidades(Sintomas)
+    ;
+    write('Paciente não encontrado, adicione ele na base para fazer um diagnóstico'), nl
+    ).
+
+listar_todos :-
+    read_pacientes(Pacientes),
+    nl, write(Pacientes), nl.
+
 
 :- begin_tests(doenca).
 
--- probabilidade.pl
+++ probabilidade.pl
@@ -6,7 +6,8 @@ test(t0) :- count_sintomas([],[], 0).
 test(t1) :- count_sintomas([1], [1], 1).
 test(t2) :- count_sintomas([1], [1,2], 1).
 test(t3) :- count_sintomas([1,2,3], [1,2,3], 3).
-test(t4) :- count_sintomas([1,2,3,4], [1,2,3,4,5,6,7,8], 4).
+test(t4) :- count_sintomas([1,2,3,4, 10], [1,2,3,4,5,6,7,8], 4).
+test(t11) :- count_sintomas([10,11,12,13], [1,2,3,4], 0).
 
 :- end_tests(count_sintomas).
 
@@ -17,12 +18,27 @@ test(t6) :- probabilidade(4,4, 1).
 
 :- end_tests(probabilidade).
 
+:- begin_tests(get_probabilidade_by_sintomas).
+
+test(t7) :- get_probabilidade_by_sintomas([1,2,3,4], [1,2,3,4,5,6,7,8], 0.5).
+test(t8) :- get_probabilidade_by_sintomas([1,2,3], [1,2,3], 1).
+test(t9) :- get_probabilidade_by_sintomas(['náusea'], ['náusea'], 1).
+test(t9) :- get_probabilidade_by_sintomas(['náusea', 'dor no braço'], ['náusea', 'dor no braço'], 1).
+
+:- end_tests(get_probabilidade_by_sintomas).
+
 count_sintomas([], _, 0).
 
 count_sintomas([SPX | SP], SD, N) :-
     count_sintomas(SP, SD, T),
-    member(SPX, SD),
-    N is T + 1.
+    (
+        member(SPX, SD) -> N is T + 1 ; N is T
+    ).
 
 probabilidade(Numerador, Denominador, Resultado) :-
-    Resultado is Numerador / Denominador.
+    Resultado is Numerador / Denominador; true.
+
+get_probabilidade_by_sintomas(SP, SD, P) :-
+    count_sintomas(SP, SD, N),
+    length(SD, L),
+    P is N / L.
